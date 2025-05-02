ff527d2fe0ce103dd33e2fa56a548102facaba89
Daste <stefankar1000@gmail.com>
2025-05-02 22:28:15 +0000

    fish: Add keybindings and vcs prompt overrides

--- home.nix
+++ home.nix
@@ -82,6 +82,19 @@
       };
       functions = {
         fish_greeting = "";
+        fish_user_key_bindings = ''
+          # Execute this once per mode that emacs bindings should be used in
+          fish_default_key_bindings -M insert
+          # Then execute the vi-bindings so they take precedence when there's a conflict.
+          # Without --no-erase fish_vi_key_bindings will default to
+          # resetting all bindings.
+          # The argument specifies the initial mode (insert, "default" or visual).
+          fish_vi_key_bindings --no-erase insert
+          set -g fish_escape_key_delay 10
+          # Edit the command buffer in $EDITOR with Ctrl+X
+          bind --mode insert \cX 'edit_command_buffer'
+        '';
+        fish_vcs_prompt = "fish_git_prompt $argv";
         last_history_item = "echo $history[1]";
         activate_venv = ''
           for venv_dir in "venv" ".venv"
