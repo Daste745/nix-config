98def987e6f86caccc3cbb8fc1ff7856c73a6fcb
Daste <stefankar1000@gmail.com>
2025-05-04 17:56:22 +0000

    Move WSL config to hosts/nauvis

--- flake.nix
+++ flake.nix
@@ -18,51 +18,7 @@
         modules = [
           nixos-wsl.nixosModules.default
           home-manager.nixosModules.home-manager
-          ({ pkgs, ... }: {
-            system.stateVersion = "24.11";
-
-            wsl = {
-              enable = true;
-              defaultUser = "stefan";
-            };
-
-            users.users.stefan = {
-              isNormalUser = true;
-              extraGroups = [ "wheel" ];
-              shell = pkgs.fish;
-            };
-
-            environment.systemPackages = with pkgs; [
-              file
-              tree
-              wget
-              htop
-              fastfetch
-              git
-              nix-output-monitor
-              gnupg
-              ripgrep
-            ];
-
-            programs.fish.enable = true;
-            programs.nix-ld.enable = true;  # For VSCode server on WSL
-
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              backupFileExtension = "backup";
-              users.stefan = import ./home.nix;
-            };
-
-            nix = {
-              settings = {
-                experimental-features = [
-                  "nix-command"
-                  "flakes"
-                ];
-              };
-            };
-          })
+          ./hosts/nauvis
         ];
       };
     };
