ce54ed4b3c7443467abbd3497e4ede749185df77
Daste <stefankar1000@gmail.com>
2025-05-12 20:50:15 +0000

    Use nixpkgs and home-manager unstable

--- flake.nix
+++ flake.nix
@@ -5,10 +5,12 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ nixpkgs, nixos-wsl, home-manager, ... }: {
