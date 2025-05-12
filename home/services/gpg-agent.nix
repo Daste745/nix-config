82767918f65ba157fb4d99abc330878523402bf7
Daste <stefankar1000@gmail.com>
2025-05-12 21:41:32 +0000

    services/gpg-agent: Enable fish integration

--- home/services/gpg-agent.nix
+++ home/services/gpg-agent.nix
@@ -32,6 +32,7 @@ in
     services.gpg-agent = {
       enable = true;
       enableSshSupport = true;
+      enableFishIntegration = true;
       extraConfig = lib.concatStringsSep "\n" extraConfigParts;
     };
   };
