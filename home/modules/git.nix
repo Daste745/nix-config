0edc9fff082c8493fa4d16060b0881d8330f3019
Daste <stefankar1000@gmail.com>
2025-05-23 20:55:00 +0000

    modules/git: Fix quoting issues in `git lg`

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -22,9 +22,7 @@
       };
     };
     aliases = {
-      # FIXME: --format="..." has escaped quotes (\") in the final output which is incorrect.
-      #        Maybe creating this as a `git-lg` program would be easier to maintain?
-      lg = ''log --graph --abbrev-commit --decorate --format="format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n'''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'"'';
+      lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
       unpushed = "log --branches --not --remotes --no-walk --decorate --oneline";
       # Find the base of the current branch, defaulting to the `GIT_DEFAULT_BRANCH` env if not provided
       branch-base = "!f() { git merge-base \${1:-\$GIT_DEFAULT_BRANCH} @; }; f";
