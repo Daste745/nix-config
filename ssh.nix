bdf5b9506210ee12109ce6069de1bc4765b55f49
Daste <stefankar1000@gmail.com>
2025-05-24 17:18:41 +0000

    ssh: Add key from nauvis

--- ssh.nix
+++ ssh.nix
@@ -1,5 +1,6 @@
 {
   publicKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP2EFXpXri1rQwyhrZbdJ9lHu96NdYZApVjfbdFY4Jca stefan@nauvis"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGvezkFdF7c5cSDYGiDPCVhq/Qhni+QOS9m/BMdPX82y stefan@aquilo"
   ];
 }
