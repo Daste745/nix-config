4fa6772988dca6fc99cf7b6cc9d6fd9050dfab88
Daste <stefankar1000@gmail.com>
2025-05-24 18:39:17 +0200

    modules/starship: Enable more modules

--- home/modules/starship.nix
+++ home/modules/starship.nix
@@ -4,7 +4,22 @@
     enableFishIntegration = true;
     settings = {
       add_newline = false;
-      shell.disabled = true;
+      username = {
+        format = "[$user]($style)@";
+        show_always = true;
+      };
+      hostname = {
+        ssh_only = false;
+        trim_at = "";
+        format = "[$hostname]($style) [$ssh_symbol]($style)in ";
+      };
+      status = {
+        disabled = false;
+        pipestatus = true;
+      };
+      git_commit.tag_disabled = false;
+      git_metrics.disabled = false;
+      direnv.disabled = false;
     };
   };
 }
--- hosts/aquilo/home.nix
+++ hosts/aquilo/home.nix
@@ -7,6 +7,7 @@
 
   home.packages = with pkgs; [
     maple-mono.truetype
+    maple-mono.NF
   ];
 
   imports = [
