f7a01025321b019c6e1627ecd8f2eb459e737c3a
Daste <stefankar1000@gmail.com>
2025-05-27 20:20:32 +0200

    services/skhd: Spawn Alacritty windows using `open`

--- home/services/skhd.nix
+++ home/services/skhd.nix
@@ -3,8 +3,7 @@
   services.skhd = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
     enable = true;
     config = ''
-      # FIXME)) Alacritty window name and icon are ugly :^(
-      ctrl + cmd - return : alacritty
+      ctrl + cmd - return : open -n -a Alacritty
     '';
   };
 }
