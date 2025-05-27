3215d3264bc58db5bfd2cc683480a7c746e43aa2
Daste <stefankar1000@gmail.com>
2025-05-27 19:19:32 +0000

    assets/ssh: Add system key for nauvis

--- assets/ssh.nix
+++ assets/ssh.nix
@@ -3,4 +3,13 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP2EFXpXri1rQwyhrZbdJ9lHu96NdYZApVjfbdFY4Jca stefan@nauvis"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGvezkFdF7c5cSDYGiDPCVhq/Qhni+QOS9m/BMdPX82y stefan@aquilo"
   ];
+
+  users = [
+    nauvis = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP2EFXpXri1rQwyhrZbdJ9lHu96NdYZApVjfbdFY4Jca stefan@nauvis";
+    aquilo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGvezkFdF7c5cSDYGiDPCVhq/Qhni+QOS9m/BMdPX82y stefan@aquilo";
+  ];
+
+  systems = [
+    nauvis = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA08w+wqyBTZJtK0roQtOuc3Ly8PBIT10SRFGqiuElrP root@nauvis";
+  ];
 }
