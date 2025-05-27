4574469b6e6ea207fc61269738d15c858a624ff1
Daste <stefankar1000@gmail.com>
2025-05-27 19:24:51 +0000

    assets/ssh: Make `users` and `systems` attribute sets

--- assets/ssh.nix
+++ assets/ssh.nix
@@ -4,13 +4,13 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGvezkFdF7c5cSDYGiDPCVhq/Qhni+QOS9m/BMdPX82y stefan@aquilo"
   ];
 
-  users = [
+  users = {
     nauvis = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP2EFXpXri1rQwyhrZbdJ9lHu96NdYZApVjfbdFY4Jca stefan@nauvis";
     aquilo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGvezkFdF7c5cSDYGiDPCVhq/Qhni+QOS9m/BMdPX82y stefan@aquilo";
-  ];
+  };
 
-  systems = [
+  systems = {
     nauvis = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA08w+wqyBTZJtK0roQtOuc3Ly8PBIT10SRFGqiuElrP root@nauvis";
     aquilo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII6H23EDMgHuReZCuvCvH7nI3rpETV/bw2ey5YpgP/FJ";
-  ];
+  };
 }
