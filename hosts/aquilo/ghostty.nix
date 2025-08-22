d240378279092822443b95e3d29878aa8fecffcb
Daste <stefankar1000@gmail.com>
2025-08-22 21:25:34 +0200

    modules/ghostty: Quit after last window is closed

--- hosts/aquilo/ghostty.nix
+++ hosts/aquilo/ghostty.nix
@@ -6,11 +6,13 @@
     enableFishIntegration = true;
     settings = {
       maximize = true;
+      # FIXME: Remove arbitrary window sizes once `maximize = true` works on MacOS
       window-height = 1000;
       window-width = 1000;
       theme = "Afterglow";
       font-family = "Maple Mono";
       font-size = "12";
+      quit-after-last-window-closed = true;
       macos-titlebar-proxy-icon = "hidden";
     };
   };
