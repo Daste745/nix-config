9a275ddba90fe04cfb432f3832a00bbade623212
Daste <stefankar1000@gmail.com>
2025-08-24 21:08:09 +0000

    hosts/thinkpad: Add basic hyprland keybinds

--- hosts/thinkpad/hyprland.nix
+++ hosts/thinkpad/hyprland.nix
@@ -1,12 +1,29 @@
+{ pkgs, ... }:
 {
-  # TODO)) ghostty
-  programs.kitty.enable = true;
+  home.packages = with pkgs; [
+    # TODO)) ghostty
+    kitty
+    xfce.thunar
+    wofi
+  ];
 
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
-      "$mod" = "SUPER";
-      exec-once = "$terminal";
+      "$mod" = "CTRL_SHIFT";
+      "$terminal" = "kitty";
+      "$fileManager" = "thunar";
+      "$menu" = "wofi --show drun";
+      bind = [
+        "$mod, Q, killactive"
+        "$mod, enter, exec, $terminal"
+        "$mod, E, exec, $fileManager"
+        "$mod, R, exec, $menu"
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocus, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
+      ];
       # TODO)) Rest of the config
     };
   };
