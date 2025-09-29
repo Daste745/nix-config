38e0305df2d6464db05e93056a2f39fec10a6f27
Daste <stefankar1000@gmail.com>
2025-09-29 15:30:40 +0200

    hosts/thinkpad: Add more xdg stuff

--- hosts/thinkpad/xdg.nix
+++ hosts/thinkpad/xdg.nix
@@ -1,6 +1,20 @@
 { pkgs, ... }:
 {
-  xdg.mime.enable = true;
+  xdg.terminal-exec = {
+    enable = true;
+    settings = {
+      default = [ "com.mitchellh.ghostty.desktop" ];
+    };
+  };
+  xdg.mime = {
+    # Mimetypes: https://developer.mozilla.org/en-US/docs/Web/HTTP/Guides/MIME_types/Common_types
+    # Data dirs: $XDG_DATA_DIRS
+    # .desktop: ls /etc/profiles/per-user/stefan/share/applications/
+    defaultApplications = {
+      "text/html" = "zen.desktop";
+      "application/pdf" = "zen.desktop";
+    };
+  };
   xdg.portal = {
     enable = true;
     xdgOpenUsePortal = true;
