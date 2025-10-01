097fb708367d0c176286b37fca25db8e118c7084
Daste <stefankar1000@gmail.com>
2025-10-01 22:32:42 +0200

    modules/fish: Go back to using nix-output-monitor on macos

--- home/modules/fish.nix
+++ home/modules/fish.nix
@@ -5,7 +5,7 @@ let
     if pkgs.stdenv.hostPlatform.isLinux then
       "sudo pixos-rebuild switch --flake ${configDir}"
     else
-      "sudo pinix --pix-command darwin-rebuild switch --flake ${configDir}";
+      "sudo darwin-rebuild switch --flake ${configDir} &| nom";
 in
 {
   home.shell.enableFishIntegration = true;
