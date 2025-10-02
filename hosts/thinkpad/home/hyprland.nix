3339f47077195003fb970140d2eeea20d878c8ff
Daste <stefankar1000@gmail.com>
2025-10-02 23:57:11 +0200

    hosts/thinkpad: Clamp volume percent up to 100

--- hosts/thinkpad/home/hyprland.nix
+++ hosts/thinkpad/home/hyprland.nix
@@ -24,7 +24,7 @@ in
     inputs.packages.${pkgs.system}.volnoti
     (pkgs.writeShellScriptBin "show-volume" ''
       VOLUME=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -d ' ' -f 2)
-      PERCENT=$(echo "scale=2; $VOLUME * 100" | ${getExe pkgs.bc})
+      PERCENT=$(echo "scale=2; $VOLUME * 100" | ${getExe pkgs.bc}| awk '{print ($1 > 100) ? 100 : $1}')
       MUTED=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -d ' ' -f 3)
       if [ "$MUTED" = "[MUTED]" ]; then
         volnoti-show -m "$PERCENT"
