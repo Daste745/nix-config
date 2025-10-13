beb4d6eb044f9229a39e24a9f970501513b981a3
Daste <stefankar1000@gmail.com>
2025-10-13 19:56:56 +0200

    templates: Remove reduntant `system` arg to the eachSystem map function

--- templates/dev-shell/flake.nix
+++ templates/dev-shell/flake.nix
@@ -8,28 +8,18 @@
     { systems, nixpkgs, ... }:
     let
       eachSystem =
-        f:
-        nixpkgs.lib.genAttrs (import systems) (
-          system:
-          f {
-            inherit system;
-            pkgs = import nixpkgs { inherit system; };
-          }
-        );
+        f: nixpkgs.lib.genAttrs (import systems) (system: f (import nixpkgs { inherit system; }));
     in
     {
-      devShells = eachSystem (
-        { system, pkgs }:
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
 
-            ];
-            shellHook = ''
+          ];
+          shellHook = ''
 
-            '';
-          };
-        }
-      );
+          '';
+        };
+      });
     };
 }
