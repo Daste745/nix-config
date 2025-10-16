b33192153cc6a0ab9232a2c66e98185bbdfb5574
Daste <stefankar1000@gmail.com>
2025-10-16 19:13:09 +0200

    modules/zed: Don't install zed and its alias in WSL

--- home/modules/zed/default.nix
+++ home/modules/zed/default.nix
@@ -17,32 +17,14 @@ in
     let
       # `pkgs.zed-editor` only emits the `zeditor` binary, but we also want an `zed`
       zeditorAlias = pkgs.writeShellScriptBin "zed" "${lib.getExe pkgs.zed-editor} $@";
-      wslCompatScript = pkgs.writeShellScriptBin "zed" ''
-        set -euo pipefail
-
-        # zed: Open a file in Zed editor on Windows using SSH remote
-        # Requires a non-development version of Zed to be in Windows' PATH
-
-        if [ $# -ne 1 ]; then
-          echo "Usage: zed <path>" >&2
-          exit 1
-        fi
-
-        remote=$(hostname)
-        path=$(readlink -f "$1")
-
-        zed.exe ssh://$remote$path & disown $!
-      '';
     in
     {
-      home.packages =
-        (lib.optionals (pkgs.stdenv.hostPlatform.isLinux && !cfg.wslCompatScript.enable) [
-          pkgs.zed-editor
-          zeditorAlias
-        ])
-        ++ (lib.optionals cfg.wslCompatScript.enable [
-          wslCompatScript
-        ]);
+      # TODO)) Change this condition to be a generic "is WSL" check.
+      #        We don't need the `zed` alias and `zed-editor` package in WSL
+      home.packages = lib.optionals (pkgs.stdenv.hostPlatform.isLinux && !cfg.wslCompatScript.enable) [
+        pkgs.zed-editor
+        zeditorAlias
+      ];
 
       xdg.configFile = {
         # FIXME)) ~/.nix-config shouldn't be hardcoded
