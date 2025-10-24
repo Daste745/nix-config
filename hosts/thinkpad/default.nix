5d0131cb1ed1e39ccc33cfc8170e3bfd751373f4
Daste <stefankar1000@gmail.com>
2025-10-24 13:49:23 +0200

    flake.lock: Update nixpkgs

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -11,6 +11,21 @@
   system.stateVersion = "25.11";
   nixpkgs.hostPlatform = "x86_64-linux";
   nixpkgs.config.allowUnfree = true;
+  # TODO)) Remove once on at least this nixpkgs commit:
+  #        https://github.com/NixOS/nixpkgs/commit/1b1e26e1b86bc1de2306d3bf4421e34a557bfea3
+  nixpkgs.overlays = [
+    (final: prev: {
+      nextcloud-client = prev.nextcloud-client.overrideAttrs (prevAttrs: rec {
+        version = "4.0.0";
+        src = prev.fetchFromGitHub {
+          owner = "nextcloud-releases";
+          repo = "desktop";
+          tag = "v${version}";
+          hash = "sha256-IXX1PdMR3ptgH7AufnGKBeKftZgai7KGvYW+OCkM8jo=";
+        };
+      });
+    })
+  ];
 
   boot.loader = {
     systemd-boot.enable = true;
