4e788eb96565f01f283e194ec94b9925dec94c85
Daste <stefankar1000@gmail.com>
2025-10-24 13:20:21 +0200

    modules/git: Use new config structure

--- home/modules/git/default.nix
+++ home/modules/git/default.nix
@@ -25,8 +25,6 @@ in
   config = {
     programs.git = {
       enable = true;
-      userName = "Daste";
-      userEmail = "stefankar1000@gmail.com";
       includes = [
         { path = "~/.user.gitconfig"; }
         { path = "${configHome}/git/user-config"; }
@@ -37,7 +35,11 @@ in
         format = "ssh";
         signByDefault = true;
       };
-      extraConfig = {
+      settings = {
+        user = {
+          name = "Daste";
+          email = "stefankar1000@gmail.com";
+        };
         init = {
           defualtBranch = "main";
         };
@@ -54,16 +56,16 @@ in
         gpg = {
           ssh.allowedSignersFile = "~/.ssh/allowed_signers";
         };
-      };
-      aliases = {
-        lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
-        unpushed = "log --branches --not --remotes --no-walk --decorate --oneline";
-        # Find the base of the current branch, defaulting to the `GIT_DEFAULT_BRANCH` env if not provided
-        branch-base = "!f() { git merge-base \${1:-\$GIT_DEFAULT_BRANCH} @; }; f";
-        branch-summary = "!f() { git diff --compact-summary \$(git branch-base \$1) @; }; f";
-        branch-diff = "!f() { git diff \$(git branch-base \$1) @; }; f";
-        # branch-summary, but grepped for `migrations` - useful for Django
-        branch-migrations = "!f() { git diff --name-only \$(git branch-base \$1) @ | rg migrations; }; f";
+        alias = {
+          lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
+          unpushed = "log --branches --not --remotes --no-walk --decorate --oneline";
+          # Find the base of the current branch, defaulting to the `GIT_DEFAULT_BRANCH` env if not provided
+          branch-base = "!f() { git merge-base \${1:-\$GIT_DEFAULT_BRANCH} @; }; f";
+          branch-summary = "!f() { git diff --compact-summary \$(git branch-base \$1) @; }; f";
+          branch-diff = "!f() { git diff \$(git branch-base \$1) @; }; f";
+          # branch-summary, but grepped for `migrations` - useful for Django
+          branch-migrations = "!f() { git diff --name-only \$(git branch-base \$1) @ | rg migrations; }; f";
+        };
       };
     };
 
