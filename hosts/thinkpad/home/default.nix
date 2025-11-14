f9ced3829d54efc0e6eadf0d13e89f936ee35809
Daste <stefankar1000@gmail.com>
2025-11-14 16:01:56 +0100

    hosts/thinkpad: Remove test nixpkgs-notify

--- hosts/thinkpad/home/default.nix
+++ hosts/thinkpad/home/default.nix
@@ -21,8 +21,6 @@ in
     thunderbird
     bruno
     obsidian
-    # TODO)) Remove - added to test the the check-nixpkgs-pr-merges workflow
-    # nixpkgs-notify #455302
     feishin
     spotify
     nextcloud-client
