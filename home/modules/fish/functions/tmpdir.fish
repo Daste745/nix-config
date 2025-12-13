75c4896dc17020707ece61f56c5cdfed60937216
Daste <stefankar1000@gmail.com>
2025-12-13 16:02:03 +0100

    modules/fish: Add a `--flake` option when creating tmpdirs

--- home/modules/fish/functions/tmpdir.fish
+++ home/modules/fish/functions/tmpdir.fish
@@ -1,5 +1,12 @@
+argparse --stop-nonopt --name "tmpdir" "flake" -- $argv
+or return
+
 set -l new_dir (mktemp -d /tmp/XXXXXXX)
 
 echo -e "$new_dir $(date -Iseconds)" >> /tmp/tmpdir.log
 
 cd $new_dir
+
+if test -n "$_flag_flake"
+    nix flake init
+end
