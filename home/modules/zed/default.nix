ea2a8ec824c9574609279356f07d19c18a271f9f
Daste <stefankar1000@gmail.com>
2026-04-15 16:21:27 +0200

    modules/zed: Add workaround to GIT_EDITOR file content retention bug
    Zed sometimes retains contents of
    `.git/{COMMIT_EDITMSG,git-rebase-todo}` files after they've been
    opened/closed multiple times, causing the commit/todo to be invalid.
    
    By using a `zed-git-editor` wrapper script which copies the commit/todo
    file to `/tmp`, zed is forced to load its content from disk every time.
    
    Moving the file back to its original location before exiting the wrapper
    lets git use these files normally.
    
    This also requires adding `/tmp/...` paths as file type associations to
    zed's config, so it'll highlight their syntax correctly.
    
    See https://github.com/zed-industries/zed/issues/29669 for more context.

--- home/modules/zed/default.nix
+++ home/modules/zed/default.nix
@@ -18,14 +18,35 @@ in
     let
       # `pkgs.zed-editor` only emits the `zeditor` binary, but we also add a `zed` alias
       zeditorAlias = pkgs.writeShellScriptBin "zed" ''exec ${lib.getExe pkgs.zed-editor} "$@"'';
-    in
-    {
+      # Wrapper around `GIT_EDITOR="zed --wait"` to fix retained COMMIT_EDITMSG file contents bug
+      # Opening a new file forces zed to load its contents from disk, mitigating the bug.
+      # TODO)) Remove once https://github.com/zed-industries/zed/issues/29669 is fixed
+      zedGitEditor = pkgs.writeShellApplication {
+        name = "zed-git-editor";
+        runtimeInputs = [ pkgs.coreutils ];
+        text = ''
+          # `basename "$1"` extracts `COMMIT_EDITMSG` or `git-rebase-todo` from the original file path
+          tmp=$(mktemp "/tmp/$(basename "$1").XXXXXX")
+          cp "$1" "$tmp"
+
+          zed --wait "$tmp"
+
+          cp "$tmp" "$1"
+          rm "$tmp"
+        '';
+      };
+
+      commonPackages = [ zedGitEditor ];
       # TODO)) Change this condition to be a generic "is WSL" check.
       #        We don't need the `zed` alias and `zed-editor` package in WSL
-      home.packages = lib.optionals (isLinux && !cfg.wslCompatScript.enable) [
+      isLinuxDesktop = isLinux && !cfg.wslCompatScript.enable;
+      linuxDesktopPackages = [
         pkgs.zed-editor
         zeditorAlias
       ];
+    in
+    {
+      home.packages = commonPackages ++ (lib.optionals isLinuxDesktop linuxDesktopPackages);
 
       xdg.configFile = {
         # FIXME)) ~/.nix-config shouldn't be hardcoded
