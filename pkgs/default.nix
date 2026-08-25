b82b317fabef913691cd86241148e1cf4b9e7660
Daste <stefankar1000@gmail.com>
2026-08-25 12:24:43 +0200

    pkgs: Simplify module

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,24 +1,18 @@
 inputs:
 let
-  lib = inputs.nixpkgs.lib;
-  mkSystemPackages =
-    system: f:
-    let
-      pkgs = inputs.nixpkgs.legacyPackages.${system};
-    in
-    f (inputs // { inherit lib pkgs; });
+  mkSystemPackages = system: f: f (inputs.nixpkgs.legacyPackages.${system}.callPackage);
 in
 {
-  x86_64-linux = mkSystemPackages "x86_64-linux" (args: {
-    volnoti = args.pkgs.callPackage ./volnoti.nix { };
-    nirimap = args.pkgs.callPackage ./nirimap.nix { };
-    gnhf = args.pkgs.callPackage ./gnhf.nix { };
-    check-flake-updates = args.pkgs.callPackage ./check-flake-updates.nix { };
-    dix-fzf = args.pkgs.callPackage ./dix-fzf.nix { };
+  x86_64-linux = mkSystemPackages "x86_64-linux" (callPackage: {
+    volnoti = callPackage ./volnoti.nix { };
+    nirimap = callPackage ./nirimap.nix { };
+    gnhf = callPackage ./gnhf.nix { };
+    check-flake-updates = callPackage ./check-flake-updates.nix { };
+    dix-fzf = callPackage ./dix-fzf.nix { };
   });
-  aarch64-darwin = mkSystemPackages "aarch64-darwin" (args: {
-    check-flake-updates = args.pkgs.callPackage ./check-flake-updates.nix { };
-    dix-fzf = args.pkgs.callPackage ./dix-fzf.nix { };
-    gnhf = args.pkgs.callPackage ./gnhf.nix { };
+  aarch64-darwin = mkSystemPackages "aarch64-darwin" (callPackage: {
+    check-flake-updates = callPackage ./check-flake-updates.nix { };
+    dix-fzf = callPackage ./dix-fzf.nix { };
+    gnhf = callPackage ./gnhf.nix { };
   });
 }
